06e5ba5c88e4d1a88d46e41946d3d81294a05e29
VengalreddyAkhila <akhila.it1801@gmail.com>
2021-05-31 14:06:32 +0530

    Refactor:Multiplication

---
+++ ConsoleApp1/Multiplication.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    abstract class Calculator
+    {
+        public abstract void Mul(int X, int Y);
+    }
+    class Multiplication : Calculator
+    {
+        public override void Mul(int X, int Y)
+        {
+            int result = X * Y;
+            Console.WriteLine($"Multiplication of {X} * {Y} = {result}");
+        }
+
+    }
+    class Division : Multiplication
+    {
+        public void Div(int X, int Y)
+        {
+            int result = X / Y;
+            Console.WriteLine($"Division of {X} / {Y} = {result}");
+        }
+    }
+}
+      
+
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,6 +7,10 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Division S = new Division();
+
+            S.Mul(2, 5);
+            S.Div(10, 5);
         }
     }
 }
